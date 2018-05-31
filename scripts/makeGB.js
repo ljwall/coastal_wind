b0f1a9c99fa0a9e9d2775c029a77ebef5b741d88
liam <liam@magicseaweed.com>
2018-05-31 14:15:12 -0700

    Add script to make GB islands as individual features in a geoJSON file

---
+++ scripts/makeGB.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+fs.readFile(__dirname+'/../public/geojson/NUTS/NUTS_RG_01M_2013_4326_LEVL_0.geojson', 'utf8', (err, json) => {
+  var data = JSON.parse(json),
+    geojsonObject = {
+      'type': 'FeatureCollection',
+      'features': []
+    };
+
+  data.features[34].geometry.coordinates.forEach((data, index) => {
+    if (index !== 1) {
+      geojsonObject.features.push({
+        type: 'Feature',
+        properties: {
+          name: 'Ind:'+index
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: data
+        }
+      });
+    }
+  });
+
+  fs.writeFile(__dirname+'/GB-parts.geojson', JSON.stringify(geojsonObject), (err) => {
+    if (err)
+      return console.log('error');
+
+    console.log('file wirtten');
+  });
+});
